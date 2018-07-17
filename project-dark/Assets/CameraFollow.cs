0e09a2bceb6445aa22a87e80e8348526ab6713e1
Denislav Georgiev <d1xt1r@gmail.com>
2018-07-17 14:41:49 +0300

    Add CameraFollow script

---
+++ project-dark/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform donut;
+    public float smoothSpeed;
+    public Vector3 offset;
+
+    void FixedUpdate() {
+        Vector3 desiredPosition = donut.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
