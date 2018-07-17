012802ffe4d518e2bb8c9d50b6ccc20495e42f66
Denislav Georgiev <d1xt1r@gmail.com>
2018-07-17 14:40:46 +0300

    Add OnTrigger script for the animation

---
+++ project-dark/Assets/AnimationOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AnimationOnTrigger : MonoBehaviour {
+
+    public List<PlayableDirector> playableDirectors;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            foreach (PlayableDirector playableDirector in playableDirectors) {
+                playableDirector.Play();
+            }
+        }
+    }
+
+}
